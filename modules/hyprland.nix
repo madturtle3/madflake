b9ef83d7846d48e9eb284a9dd7eda5ed1c1f1ab7
Mason Braithwaite <mason@braith.net>
2025-08-02 17:55:28 -0600

    added BLE to hyprland

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,5 +1,16 @@
 { config, pkgs, ... }:
 {
+  # BLE configuration
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true; # Show battery charge of Bluetooth devices
+      };
+    };
+  };
+  services.blueman.enable = true;
   programs.hyprland.enable = true;
   environment.systemPackages = with pkgs; [
     chezmoi # for the dots because I am done with this
@@ -8,13 +19,14 @@
     hyprpaper
     bibata-cursors
     waybar
-    wofi
+    rofi-wayland
+    hyprlock
+    hypridle
     pavucontrol
+    bluema
   ];
-  fonts.packages =
-    with pkgs;
-    [
-      nerd-fonts.symbols-only
-      noto-fonts-color-emoji
-    ];
+  fonts.packages = with pkgs; [
+    nerd-fonts.symbols-only
+    noto-fonts-color-emoji
+  ];
 }
